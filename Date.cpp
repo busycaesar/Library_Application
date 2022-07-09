dfef2d23f87c5ce28ac1f478004759420a3fea0f
Dev Shah <busycaesar@gmail.com>
2022-07-09 17:22:30 -0400

    Initial Commit

---
+++ Date.cpp
@@ -0,0 +1,317 @@
+// Final Project Milestone 1
+// Date Module
+// File  Date.cpp
+// Version 1.0
+// Author   Fardad Soleimanloo
+// Revision History
+// -----------------------------------------------------------
+// Name               Date                 Reason
+/////////////////////////////////////////////////////////////////
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iomanip>
+#include <iostream>
+#include <ctime>
+#include "Date.h"
+
+using namespace std;
+
+namespace sdds
+{
+
+	// THIS WILL SET THE OBJECT TO EMPTY STATE.
+	void Date::setEmpty()
+	{
+
+		m_year = 0;
+		m_mon = 0;
+		m_day = 0;
+		m_errorCode = 0;
+		m_currentYear = systemYear();
+
+	}
+
+	// THIS WILL RETURN TOTAL NUMBER OF DAYS SINCE 0001 / 01 / 01.
+	int Date::daysSince0001_1_1()const
+	{
+
+		// VARIABLE DECLARATION.
+		int ty = m_year;
+		int tm = m_mon;
+
+		if (tm < 3)
+		{
+
+			ty--;
+			tm += 12;
+
+		}
+
+		return 365 * ty + ty / 4 - ty / 100 + ty / 400 + (153 * tm - 457) / 5 + m_day - 306;
+
+	}
+
+	// THIS WILL RETURN TRUE IF (YEAR, MONTH, AND DAY) IS IN PROPER RANGE. OTHERWISE, FALSE.
+	bool Date::validate()
+	{
+
+		errCode(NO_ERROR);
+
+		if (m_year < MIN_YEAR || m_year > m_currentYear + 1)
+		{
+
+			errCode(YEAR_ERROR);
+
+		}
+		else if (m_mon < 1 || m_mon > 12)
+		{
+
+			errCode(MON_ERROR);
+
+		}
+		else if (m_day < 1 || m_day > mdays())
+		{
+
+			errCode(DAY_ERROR);
+
+		}
+
+		return !bad();
+
+	}
+
+	// THIS WILL ASSIGN DATA MEMBER 'm_errorCode' A VALUE AS PER THE ERROR.
+	void Date::errCode(int readErrorCode)
+	{
+
+		m_errorCode = readErrorCode;
+
+	}
+
+	int Date::systemYear()const
+	{
+
+		// VARIABLE DECLARATION.
+		time_t t = time(NULL);
+		tm lt = *localtime(&t);
+
+		return lt.tm_year + 1900;
+
+	}
+
+	// THIS WILL RETURN TRUE IF THERE IS NO ERROR IN THE DATES. OTHERWISE FALSE.
+	bool Date::bad()const
+	{
+
+		return m_errorCode != 0;
+
+	}
+
+	// THIS WILL RETURN THE NUMBER OF DAYS IN THE CURRENT MONTH.
+	int Date::mdays()const
+	{
+
+		// VARIABLE DECLARATION.
+		int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, -1 };
+		int mon = m_mon >= 1 && m_mon <= 12 ? m_mon : 13;
+
+		mon--;
+
+		return days[mon] + int((mon == 1) * ((m_year % 4 == 0) && (m_year % 100 != 0)) || (m_year % 400 == 0));
+
+	}
+
+	// THIS WILL ASSIGN THE DATA MEMBERS VALUE AS PER CURRENT DATE.
+	void Date::setToToday()
+	{
+
+		// VARIABLE DECLARATION.
+		time_t t = time(NULL);
+		tm lt = *localtime(&t);
+
+		m_day = lt.tm_mday;
+		m_mon = lt.tm_mon + 1;
+		m_year = lt.tm_year + 1900;
+		errCode(NO_ERROR);
+
+	}
+
+	// THIS WILL BUILD AN OBJECT AND ASSIGN THE DATA MEMBERS VALUE AS PER CURRENT DATE.
+	Date::Date() :m_currentYear(systemYear())
+	{
+
+		setEmpty();
+		setToToday();
+
+	}
+
+	// THIS WILL BUILD AN OBJECT AND ASSIGN THE DATA MEMBERS VALUE AS PER THE RECEIVED ARGUMENT.
+	Date::Date(int year, int mon, int day) : m_currentYear(systemYear())
+	{
+
+		// VARIABLE DECLARATION.
+		m_year = year;
+		m_mon = mon;
+		m_day = day;
+
+		validate();
+
+	}
+
+	// THIS WILL RETURN CURRENT ERROR CODE.
+	int Date::errCode()const
+	{
+
+		return m_errorCode;
+
+	}
+
+	// THIS WILL RETURN THE DESCRIPTION OF THE ERROR ON THE BASIS OF ITS CODE.
+	const char* Date::dateStatus()const
+	{
+
+		return DATE_ERROR[errCode()];
+
+	}
+
+	// THIS WILL RETURN THE VALUE OF DATA MEMBER 'm_currentYear'.
+	int Date::currentYear()const
+	{
+
+		return m_currentYear;
+
+	}
+
+	istream& Date::read(istream& is = cin)
+	{
+
+		// VARIABLE DECLARATION.
+		char year[5], month[3], date[3];
+
+		// SETTING THE ERROR CODE TO 0.
+		errCode(NO_ERROR);
+
+		is.get(year, 4);
+		is.ignore();
+		is.get(month, 2);
+		is.ignore();
+		is.get(date, 2, '\n');
+
+		m_year = atoi(year);
+		m_mon = atoi(month);
+		m_day = atoi(date);
+
+		if (is.fail())
+		{
+
+			errCode(CIN_FAILED);
+
+		}
+
+		validate();
+
+		is.clear();
+
+		return is;
+
+	}
+
+	ostream& Date::write(ostream& os = cout)const
+	{
+
+		// IF THE DATE VALUES ARE INVALID, THIS WILL PRINT THE DESCRIPTION OF THE ERROR.  
+		if (bad())
+		{
+
+			os << dateStatus();
+
+		}
+		else
+		{
+
+			os << m_year << "/" << setw(2) << setfill('0') << right << m_mon << "/" << setw(2) << setfill('0') << right << m_day << setfill(' ');
+
+		}
+
+		return os;
+
+	}
+
+	bool Date::operator ==(Date& RO)
+	{
+
+		return (m_day == RO.m_day && m_mon == RO.m_mon && m_year == RO.m_year);
+
+	}
+
+	bool Date::operator !=(Date& RO)
+	{
+
+		return m_day != RO.m_day;
+
+	}
+
+	bool Date::operator >=(Date& RO)
+	{
+
+		return(*this > RO || *this == RO);
+
+	}
+
+	bool Date::operator <=(Date& RO)
+	{
+
+		return(*this < RO || *this == RO);
+
+	}
+
+	bool Date::operator <(Date& RO)
+	{
+
+		return ((m_year <= RO.m_year) && (m_mon <= RO.m_mon) && (m_day < RO.m_day));
+
+	}
+
+	bool Date::operator >(Date& RO)
+	{
+
+		return ((m_year >= RO.m_year) && (m_mon >= RO.m_mon) && (m_day > RO.m_day));
+
+	}
+
+	int Date::operator -(Date& RO)
+	{
+
+		return daysSince0001_1_1() - RO.daysSince0001_1_1();
+
+	}
+
+	Date::operator bool()
+	{
+
+		return validate();
+
+	}
+
+	ostream& operator<<(ostream& os, const Date& RO)
+	{
+
+		return RO.write(os);
+
+	}
+
+	istream& operator>>(istream& is, Date& RO)
+	{
+
+		return RO.read(is);
+
+	}
+
+	// ================================== XXXXXXXXXXXXXXXXXXXXXXXXXXX =======================================
+	// ================================== XXXXXXXXXXXXXXXXXXXXXXXXXXX =======================================
+	// ================================== XXXXXXXXXXXXXXXXXXXXXXXXXXX =======================================
+	// ================================== XXXXXXXXXXXXXXXXXXXXXXXXXXX =======================================
+	// ================================== XXXXXXXXXXXXXXXXXXXXXXXXXXX =======================================
+
+}
---
+++ Date.h
@@ -0,0 +1,90 @@
+// Final Project Milestone 1
+// Date Module
+// File  Date.h
+// Version 1.0
+// Author   Fardad Soleimanloo
+// Revision History
+// -----------------------------------------------------------
+// Name               Date                 Reason
+/////////////////////////////////////////////////////////////////
+
+#ifndef SDDS_DATE_H__
+#define SDDS_DATE_H__
+
+#include <iostream>
+
+using namespace std;
+
+namespace sdds
+{
+
+	// VARIABLE DECLARATION.
+	const int NO_ERROR = 0;
+	const int CIN_FAILED = 1;
+	const int YEAR_ERROR = 2;
+	const int MON_ERROR = 3;
+	const int  DAY_ERROR = 4;
+	const char DATE_ERROR[5][16] = {
+	   "No Error",
+	   "cin Failed",
+	   "Bad Year Value",
+	   "Bad Month Value",
+	   "Bad Day Value"
+	};
+	const int  MIN_YEAR = 1500;
+
+	// CLASS.
+	class Date
+	{
+
+	private:
+
+		// DATA MEMBERS.
+		int m_year;
+		int m_mon;
+		int m_day;
+		int m_errorCode;
+		int m_currentYear;
+
+		// PRIVATE MEMBER FUNCTIONS.
+		void setEmpty();
+		int daysSince0001_1_1() const;
+		bool validate();
+		void errCode(int);
+		int systemYear() const;
+		bool bad()const;
+		int mdays()const;
+		void setToToday();
+
+	public:
+
+		// CONSTRUCTORS.
+		Date();
+		Date(int year, int mon, int day);
+
+		// PUBLIC MEMBER FUNCTIONS.
+		int errCode()const;
+		const char* dateStatus()const;
+		int currentYear()const;
+		istream& read(istream& is);
+		ostream& write(ostream& os)const;
+
+		// MEMBER OPERATORS.
+		bool operator ==(Date& RO);
+		bool operator !=(Date& RO);
+		bool operator >=(Date& RO);
+		bool operator <=(Date& RO);
+		bool operator <(Date& RO);
+		bool operator >(Date& RO);
+		int operator -(Date& RO);
+		operator bool();
+
+	};
+
+	// HELPER OPERATORS.
+	ostream& operator<<(ostream& os, const Date& RO);
+	istream& operator>>(istream& is, Date& RO);
+
+}
+
+#endif
