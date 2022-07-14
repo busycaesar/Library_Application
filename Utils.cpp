c1f3f4785f3c162d6278336d05daab23aba21371
Dev Shah <busycaesar@gmail.com>
2022-07-14 12:41:53 -0400

    Before Debugging

--- Utils.cpp
+++ Utils.cpp
@@ -1,48 +1,50 @@
-/***********************************************************************
-// OOP244 Utils Module:
-// File  utils.cpp
-// Version
-// Date
-// Author
-// Description
+///***********************************************************************
+//// OOP244 Utils Module:
+//// File  utils.cpp
+//// Version
+//// Date
+//// Author
+//// Description
+////
+//// Revision History
+//// -----------------------------------------------------------
+//// Name            Date            Reason
+///////////////////////////////////////////////////////////////////
+//***********************************************************************/
 //
-// Revision History
-// -----------------------------------------------------------
-// Name            Date            Reason
-/////////////////////////////////////////////////////////////////
-***********************************************************************/
-
-#include <iostream>
-#include "Utils.h"
-
-using namespace std;
-
-namespace sdds
-{
-
-	// FUNCTION DEFINATION.
-	int prompt(int min, int max)
-	{
-
-		// VARIABLE DECLARATION.
-		int selection = 0, temp = 0;
-
-		do
-		{
-
-			if (temp)
-			{
-
-				cout << "Invalid Selection, try again: ";
-
-			}
-
-			cin >> selection;
-
-		} while (selection >= min && selection <= max);
-
-		return selection;
-
-	}
-
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <iostream>
+//#include "Utils.h"
+//
+//using namespace std;
+//
+//namespace sdds
+//{
+//
+//	// FUNCTION DEFINATION.
+//	int prompt(int min, int max)
+//	{
+//
+//		// VARIABLE DECLARATION.
+//		int selection = 0, temp = 0;
+//
+//		do
+//		{
+//
+//			if (temp)
+//			{
+//
+//				cout << "Invalid Selection, try again: ";
+//
+//			}
+//
+//			cin >> selection;
+//
+//		} while (selection >= min && selection <= max);
+//
+//		return selection;
+//
+//	}
+//
+//}
--- Utils.h
+++ Utils.h
@@ -1,26 +1,26 @@
-/***********************************************************************
-// OOP244 Utils Module
-// File  utils.h
-// Version
-// Author
-// Description
+///***********************************************************************
+//// OOP244 Utils Module
+//// File  utils.h
+//// Version
+//// Author
+//// Description
+////
+//// Revision History
+//// -----------------------------------------------------------
+//// Name            Date            Reason
+///////////////////////////////////////////////////////////////////
+//***********************************************************************/
 //
-// Revision History
-// -----------------------------------------------------------
-// Name            Date            Reason
-/////////////////////////////////////////////////////////////////
-***********************************************************************/
-
-#ifndef SDDS_UTILS_H__
-#define SDDS_UTILS_H__
-
-#include <iostream>
-
-namespace sdds
-{
-
-	// FUNCTION SIGNATURE.
-	int prompt(int min, int max);
-
-}
-#endif // !
+//#ifndef SDDS_UTILS_H_
+//#define SDDS_UTILS_H_
+//
+//#include <iostream>
+//
+//namespace sdds
+//{
+//
+//	// FUNCTION SIGNATURE.
+//	int prompt(int min, int max);
+//
+//}
+//#endif // SDDS_UTILS_H_
